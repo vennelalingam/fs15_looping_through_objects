edadbdc44f4d57f7015e279505097492a9ac02c6
vennelalingam <lingamvennela@gmail.com>
2023-05-05 15:38:36 +0530

    looping through objects

---
+++ class.js
@@ -0,0 +1,250 @@
+// const person = {
+//     name:   "Mia",
+//     age:    30,
+//     isCoder: {
+//         javascript: true,
+//         python: false
+//     }
+// };
+
+// ////////////// Accessing Keys with for-in loop //////////////////////
+
+// for (let key in person) {
+//     console.log(key);
+// }
+
+// // name
+// // age
+// // isCoder
+
+// /////  output KEYS in an array:  ["name", "age", "isCoder"]
+
+// const arrayDetails = [];
+// for (let key in person) {
+//     arrayDetails.push(key);
+// }
+//     console.log(arrayDetails);
+
+// /////////// Built-in Object Method ////////////
+
+// // this method outputs an array with all the keys
+
+// const result = Object.keys(person);
+// console.log(result);
+// // ['name', 'age', 'isCoder']
+
+// // same to access values in an array
+
+// const result1 = Object.values(person);
+// console.log(result1);
+
+// /////////////////// Accessing Values with for-in loop ///////////////
+
+// for (let key in person) {
+//     console.log(key, person[key]);    
+// }
+
+// // Mia
+// // 30
+// // {javascript: true, python: false}
+
+// ////////// with Arrays /////////
+
+// const myArray = ["a", "b", "c"]
+// for(const key in myArray) {
+//     console.log(key, myArray[key]);
+// }
+
+// // 0 a
+// // 1 b
+// // 2 c
+//-------------------------------------------------------------------------------------------------------
+//  practice
+// /////////// for...of ///////////
+
+// const array1 = ['a', 'b', 'c']
+// for (const element of array1) {
+// console.log(element);
+// }
+
+// // a
+// // b
+// // c
+// ///////////////////// Iterating over an Array /////////////////////////
+// const numArray = [10, 20, 30];
+// for(let num of numArray) {
+//   num = num + 1;
+//   console.log(num);
+//   console.log(numArray); // [10, 20, 30];
+// }
+// console.log(numArray); // [10, 20, 30];
+// // 11
+// // 21
+// // 31
+// // Each iteration creates a new variable. Reassigning the variable inside 
+// // the loop body does not affect the original value in the iterable (an array in this case)
+
+// const array2 = [10, 20, 30]
+// for ( const values of array2) {
+//   console.log(values)
+// }
+
+// // 10
+// // 20
+// // 30
+
+// ////////////////////// Iterating over a String ////////////////////
+// const word = "boo"
+// for (const letters of word) {
+//   console.log(letters.toUpperCase());
+// }
+// // b
+// // o 
+// // o
+
+// //////////////// Iterating over a Map /////////////////////
+// const aray = new Map(
+//   [
+//     ["a", 1],
+//     ["b", 2],
+//     ["c", 3],
+//   ]
+// );
+
+// for(const entry of aray) {
+//   console.log(entry);
+// }
+
+// for (const [key, value] of aray){
+//   console.log(value);
+// }
+
+// ///////////////////// Iterating over a Set //////////////////////
+// const arr = new Set(
+//   [1, 1, 2, 2, 3, 3]
+// )
+
+// for( const value of arr) {
+//   console.log(value);
+// }
+
+
+// const friends = {
+//   a: {
+//     name: "Pedro",
+//     relationship: "school",
+//     age: 30
+//   },
+//   b: {
+//     name: "Sofia",
+//     relationship: "work",
+//     age: 38
+//   },
+//   c: {
+//     name: "Mia",
+//     relationship: "school",
+//     age: 40
+//   },
+//   d: {
+//     name: "Lucy",
+//     relationship: "work",
+//     age: 27
+//   }
+// }
+
+// /******************************************************************
+// Standard Code for lopping through any no of Objects using for..in
+// ******************************************************************/
+
+// const isObject = function(values) {
+//   if(values === null) {
+//     return false;
+//   }
+//   return(typeof values === 'object');
+// };
+
+// const objProps = function(obj) {
+//   for(let values in obj) {
+//     if(isObject(obj[values])) {
+//       objProps(obj[values]);
+//       } else {
+//       console.log(values, obj[values]);
+//     }
+//   }
+// }
+// objProps(friends);
+
+// /******************************************************************
+
+// // looping through nested objects = 2 obj.. 
+
+// // const isObject = function(values) {
+// //   if(values === null) {
+// //     return false;
+// //   }
+// //   return(typeof values === 'object');
+// // };
+
+
+// // for( let values in friends) {
+// //   //checking whether the nested child is an object..?
+// //   // if yes do this.. else console.log
+// //   if(isObject(friends[values])) {
+// //     for( let details in friends[values]) {
+// //       console.log(details, friends[values][details]);
+// //     }
+// //   } else {
+// //     console.log(values, friends[values])
+// //   }  
+// // }
+
+// ***********************************************************************/
+// // let abcd = Object.values(friends);
+// // console.log(abcd);
+// // for(const key in Object.keys(friends)){
+// //   console.log(`${key} => ${friends[key]}`);
+// // }
+// /*
+// --------------------------------------------------
+// 0:{name: 'Pedro', relationship: 'school', age: 30}
+// 1:{name: 'Sofia', relationship: 'work', age: 38}
+// 2:{name: 'Mia', relationship: 'school', age: 40}
+// 3:{name: 'Lucy', relationship: 'work', age: 27}
+// --------------------------------------------------
+
+// for (let update in friendsAges){
+//  friendsAges[update] = friendsAges[update]+1 
+//   console.log(friendsAges[update]);
+// }
+// */
+// // let abcd = Object.values(friends);
+// // console.log(abcd);
+// // for(const key in Object.keys(friends)){
+// //   console.log(`${key} => ${friends[key]}`);
+
+// //   for (let update in friends) {
+// //     friends[update] = friends[update]+1
+// //     console.log(friends[update]);
+// //   }
+// // }
+// // console.log(abcd.find(value => value.age))
+// // console.log(abcd)
+// // let details = Object.values(abcd)
+// // console.log(details);
+// // let details = Object.entries(friends);
+// // console.log(details);
+// // for (const key in friends) {
+// //   if(friends.hasOwnProperty(key)) {
+// //     console.log(`${key}: ${friends[key]}`);
+// //   }
+// // }
+
+// // for (let friend in friends) {
+// //   for (let features in friends[friend]) {
+// //     let ageFeature = friends[friend][features]
+// //     if (typeof ageFeature == 'number') {ageFeature = ageFeature +1};
+// //     console.log(ageFeature);
+// //   }
+// // }
+
+ 
---
+++ index.js
@@ -0,0 +1,233 @@
+// /*------------------
+
+
+// 1)
+// The loop is currently printing
+// the keys of the person object.
+// Switch it so it is printing
+// the values instead.
+// ------------------*/
+const person = {
+  name: "Sofia",
+  age: 30,
+  location: "Barcelona",
+  hairColor: "brown",
+  coder: true
+}
+
+for (let values in person) {
+  console.log(person[values]);
+}
+
+// /*------------------
+// 2)
+// Write a loop that prints all the values
+// in pet: ("Spot", 2, "dog", "brown").
+// ------------------*/
+
+// const pet = {
+//   name: "Spot",
+//   age: 2,
+//   animal: "dog",
+//   color: "brown",
+// }
+// for (let features in pet) {
+// //  console.log("pet features : ", pet[features]);
+// // console.log(`${features}: ${pet[features]}`)
+// console.log(`${pet[features]}`)
+// } 
+// /*------------------
+// 3)
+// Add at least FOUR properties
+// to the car object.
+
+// Then, create a loop that prints
+// the values of the car object
+// to the console.
+// ------------------*/
+
+// // const car = {
+// //   brand: "Ford",
+// //   length: "3941",
+// //   built: "strong",
+// //   color: "red",
+// // }
+// // for (let features in car) {
+// // console.log(car[features]);
+// // }
+
+// /*------------------
+// 4)
+// Now, let's print all the features
+// in ONE statement instead of one at
+// a time. Using a loop, add each value
+// to the "carFeatures" string.
+
+// Bonus #1: add a comma between each feature
+// so it more closely resembles a real
+// sentence when it prints.
+
+// Bonus #2: ensure that each feature is comma-separated but the entire sentence ends with a '.'.
+// For example: 'white, citroen, c1, 2016.'
+// Hint - For this step you'll need to check if we are on the last key and if so, treat it differently than we did in Bonus #1.  
+// ------------------*/
+
+// // let carFeatures = " "
+
+// // for (let features in car) {
+// //  carFeatures = (carFeatures + car[features] + "," + " ")
+// // console.log(`${carFeatures}.`);
+// // }
+
+// // for (const key in address) {
+// //   if (address.line1.call(address, key)) {
+// //     const element = address[key];    
+// //   }
+// /*------------------
+// 5)
+// Create a loop that adds each
+// component of the adddress object
+// to the fullAddress string.
+// ------------------*/
+
+// // let address = {
+// //   line1: "Carrer de CodeOp",
+// //   line2: 42,
+// //   city: "Barcelona",
+// //   country: "Spain"
+// // }
+
+// // let fullAddress = " "
+
+// // for (let details in address){
+// //   fullAddress =(fullAddress + address[details] + "," + " ")
+// //  console.log(`${fullAddress}.`);
+// // }
+
+// // for (const key in address) {
+// //   if (address.line1.call(address, key)) {
+// //     const element = address[key];    
+// //   }
+// // }
+
+
+// /*------------------
+// 6)
+// Write a loop that updates
+// all values that are strings to
+// uppercase.
+
+// Hint: use "typeof" to determine
+// if the value is a string, and use
+// ".toUpperCase()" to convert the string
+// to uppercase.
+// ------------------*/
+
+// const car = {
+//   brand: "ford",
+//   length: "3941",
+//   built: "strong",
+//   color: "red",
+// }
+// for (let features in car) {
+// // console.log(car[features]);
+
+//   typeof((car[features]) === "") ?
+//     console.log(car[features].toUpperCase()) : null
+//   // console.log(typeof(car[features]))
+// } 
+
+// /*------------------
+// 7)
+// Write a loop that updates
+// everyone's age by one year.
+// ------------------*/
+
+// const friendsAges = {
+//   pedro: 30,
+//   sofia: 38,
+//   mia: 40,
+//   lucy: 27
+// }
+
+// for (let update in friendsAges){
+//  friendsAges[update] = friendsAges[update]+1 
+//   console.log(friendsAges[update]);
+// }
+
+// /*------------------
+// 8)
+// Now, do the same thing– but
+// this time, you'll have to access
+// age from within each object.
+
+// It's already been started for you.
+// Hint: Use "typeof" to determine
+// if the feature is a number. If it
+// is, increase the value by 1.
+// ------------------*/
+
+
+// /*------------------
+// 9)
+// Who are the work friends, and who
+// are the school friends? Create a
+// loop that populates the two strings.
+// ------------------*/
+
+// let workFriends = "";
+// let schoolFriends = "";
+// for (let friend in friends) {
+//   for (let features in friends[friend]) {
+//     let relationship = friends[friend][features]
+//     console.log(relationship)
+//     if(relationship === "work"){
+//       console.log(workFriends)
+//     } else console.log(schoolFriends)
+// //       ((relationship) === "work") ?
+// // //     //  else if (typeof workFriends == "school") {}
+// // //     // console.log(typeof(workFriends))
+// //      console.log({workFriends}) : console.log({schoolFriends})
+    
+//     // console.log(workFriends);
+//   }
+// }
+
+// // console.log(`The person's work friends are ${workFriends} and school friends are ${schoolFriends}.`)
+
+// /*------------------
+// 10)
+// Loop through the chores object and
+// add the name of the chore to the
+// day that it is associated with.
+// When you are finished, print "mondayChores"
+// and "wednesdayChores" to the console.
+
+// Try to make what you print as natural as possible–
+// for example: "Monday's chores are: tidying,
+// vacuuming, bills."
+
+// ------------------*/
+
+// let chores = {
+//   tidying: "Monday",
+//   vaccuuming: "Monday",
+//   dishes: "Wednesday",
+//   laundry: "Wednesday",
+//   bills: "Monday",
+// }
+
+
+// for (let mondayChores in chores) {
+//  if(chores[mondayChores] === "Monday") {
+//   console.log("Monday's chores are: ", mondayChores);
+//  }
+// }
+
+// // let wednesdayChores = "";
+// // console.log(wednesdayChores)
+// for (let wednesdayChores in chores) {
+//   if(chores[wednesdayChores] === "Wednesday"){
+//   console.log("Wednesday's chores are: ", wednesdayChores);
+// }
+// }
